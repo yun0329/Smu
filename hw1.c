50812029f3c0aa9de73e3a6c46add4229a81cab6
yun0329 <yunjin0329@naver.com>
2021-09-17 19:19:26 +0900

    Add Text File [hw1.c]

---
+++ hw1.c
@@ -0,0 +1,7 @@
+﻿빌드 시작...
+1>------ 빌드 시작: 프로젝트: hw1.c, 구성: Debug Win32 ------
+1>hw1.c
+1>C:\Users\yunji\source\repos\hw1.c\hw1.c\hw1.c(8,2): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>hw1.c.vcxproj -> C:\Users\yunji\source\repos\hw1.c\Debug\hw1.c.exe
+1>"hw1.c.vcxproj" 프로젝트를 빌드했습니다.
+========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========
